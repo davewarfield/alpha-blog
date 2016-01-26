234cc91cc7268f40dd1defa8b1c63469c4f0853f
David Warfield <warfield.dave@gmail.com>
2016-01-26 22:12:23 +0000

    Add association between user and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect
     #
     @article = Article.new(article_params)
+    @article.user = User.first
 
     # Next lines done first, but no validation
     #@article.save
